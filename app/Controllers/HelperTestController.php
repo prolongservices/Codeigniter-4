8e97287ce228832368cf48716448fa05584ce36a
Pawneshwer Gupta <pawneshwergupta@gmail.com>
2020-09-02 12:42:37 +0530

    helpers

--- app/Controllers/HelperTestController.php
+++ app/Controllers/HelperTestController.php
@@ -4,13 +4,15 @@ class HelperTestController extends BaseController
 {
   public function __construct() {
     //helper('date');
-    helper('filesystem');
+    //helper('filesystem');
+    helper('pawn');
   }
 
   public function show()
   {
     //print_r(now());
     //print_r(directory_map('.'));
-    //echo write_file('./prolong.txt', 'Creating file using filesystem helper');
+    //echo write_file('./prolong.txt', 'created using filesystem helper function');
+    print_r(test_ran());
   }
 }
---
+++ app/Helpers/pawn_helper.php
@@ -0,0 +1,8 @@
+<?php
+
+if (! function_exists('test_ran'))
+{
+  function test_ran() {
+    return 123;
+  }
+}
